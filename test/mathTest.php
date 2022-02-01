592a5a2a95fb1cff0620643899336157d61f9cad
mingaha <56760594+mingaha@users.noreply.github.com>
2022-02-01 10:06:03 +0100

    Update mathTest.php

--- test/mathTest.php
+++ test/mathTest.php
@@ -13,31 +13,14 @@ class mathTest extends TestCase
 {
     function testDiviser1()
     {
-        
+
         $math = new math();
         $Resultat= $math->division(10,5);
         $this->assertEquals(2,$Resultat);
-        // echo($Resultat);
-        // if ($Resultat != 2) {
-        //     echo("la division devrait retourner deux");
-        // }
-        // echo("possible");
+     
     }
 
-    // function testDiviser()
-    // {
-    //     $math = new math();
-    //     try {
-    //        $math->division(10,0);
-    //             $this->fail("ca aurait du planter");
-
-
-    //     } catch (Exception $e) {
-    //         $this->assertEquals($e, "une erreur","ya gar");
-            
-    //     }
-    // }
-
+    
 }
 
 ?>
